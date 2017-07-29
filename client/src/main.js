0d603aaa4e8d71cba6f1900c8b496a6b278208ed
broerjuang <juangwiantoro@gmail.com>
2017-07-30 01:05:20 +0700

    adding global style

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+// @flow
+import React from 'react';
+import {render} from 'react-dom';
+
+function App() {
+  return (
+    <div className="gray">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('root'));
