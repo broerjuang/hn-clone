736e0d5eff4f09097239fb4702ec70cddba94c59
broerjuang <juangwiantoro@gmail.com>
2017-07-29 17:21:31 +0700

    configure main routes to get graphiql endPoint

--- server/src/main.js
+++ server/src/main.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+// @flow
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+
+import schema from './schema/schema.js';
+
+const PORT = process.env.PORT || 3000;
+
+function startServer() {
+  let app = express();
+
+  app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+
+  app.use(
+    '/graphiql',
+    graphiqlExpress({
+      endpointURL: '/graphql',
+    })
+  );
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}/graphiql`);
+  });
+}
+
+startServer();
