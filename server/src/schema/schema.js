4dcc875bcdc9d658b7e11d37cab10930142f72da
broerjuang <juangwiantoro@gmail.com>
2017-07-29 16:31:26 +0700

    creating executable schema for Links

--- server/src/schema/schema.js
+++ server/src/schema/schema.js
@@ -1,1 +1,13 @@
 // @flow
+
+import {makeExecutableSchema} from 'graphql-tools';
+
+let typeDefs = `
+  type Link {
+    id: ID!
+    url: String!
+    description: String!
+  }
+`;
+
+export default makeExecutableSchema({typeDefs});
